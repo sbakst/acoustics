8393e7df7b95a8f582f4a5c4234a2851b9f345b6
Sarah Bakst <bakst.sarah@gmail.com>
2018-06-11 15:34:06 -0500

    added bark

--- tamil_hack_analysis.R
+++ tamil_hack_analysis.R
@@ -26,7 +26,7 @@ tam23rds_out<-subset(tam23rds,buffer=='outbuf')
 tam23rds_in$group<-cut(tam23rds_in$trial, 18)
 ar18_med<-aggregate(cbind(f1,f1b,f2,f2b,f3,f3b)~group+sub, data = tam23rds_in, median)
 
-mindf<-NULL
+mindf3<-NULL
 for(i in (1:length(levels(tam23rds_in$sub)))){
   subj = levels(tam23rds_in$sub)[i]
   tempar<-NULL
@@ -46,26 +46,27 @@ for(i in (1:length(levels(tam23rds_in$sub)))){
 f2match = (tempar2$f2[which(tempar2$trial==trialf3)])
 arf2base = median(subset(tempar2, trial<16)$f2)
 #    cat("For subject", subj, "here's the minimum f3:", minf3,"at trial", trial, "where f2 was", f2match,"\n")
-mindf$sub[i]<-(subj)
-mindf$mintrialf3[i]<-as.numeric(trialf3)
-mindf$minf3[i]<-as.numeric(minf3)
-mindf$f2match[i]<-as.numeric(f2match)
-mindf$arf2base[i]<-as.numeric(arf2base) # what is the basleine f2 in the f3 block
-mindf$basef3[i]<-as.numeric(mbf3)
+mindf3$sub[i]<-(subj)
+mindf3$mintrialf3[i]<-as.numeric(trialf3)
+mindf3$minf3[i]<-as.numeric(minf3)
+mindf3$f2match[i]<-as.numeric(f2match)
+mindf3$arf2base[i]<-as.numeric(arf2base) # what is the basleine f2 in the f3 block
+mindf3$basef3[i]<-as.numeric(mbf3)
 }
 
-mindf<-data.frame(mindf)
+mindf3<-data.frame(mindf3)
 
 
 
-mindf$f3max<-mindf$basef3+300
+mindf3$f3max<-mindf3$basef3+300
 
-mindf$f3diff<-mindf$basef3-mindf$minf3
-mindf$f2diff<-mindf$arf2base-mindf$f2match
 
-mindf$f3perc<-(mindf$f3diff/300)
+mindf3$f3diff<-mindf3$basef3-mindf3$minf3
+mindf3$f2diff<-mindf3$arf2base-mindf3$f2match
 
-mindf$oppf2d<-mindf$f2match-mindf$arf2base
+mindf3$f3perc<-(mindf3$f3diff/300)
+
+mindf3$oppf2d<-mindf3$f2match-mindf3$arf2base
 
 
 
@@ -76,6 +77,55 @@ mindf$oppf2d<-mindf$f2match-mindf$arf2base
 
 f3inbuf<-subset(tam23rds, buffer=='inbuf')
 f3outbuf<-subset(tam23rds, buffer=='outbuf')
+
+
+
+##########
+## BARK ##
+##########
+
+mindf3b<-NULL
+for(i in (1:length(levels(tam23rds_in$sub)))){
+  subj = levels(tam23rds_in$sub)[i]
+  tempar<-NULL
+  tempar2<-NULL
+  minf3b<-NULL
+  trif3b<-NULL
+  mbf3b<-NULL
+  tempar<-subset(ar18_med, sub == subj)
+  tempar2<-subset(tam23rds_in, sub == subj)
+  minf3b = min(tempar$f3b[4:15])
+  mbf3b = median(subset(tempar2, trial<16)$f3b)  
+  trif3b = (tempar2$trial[which(tempar2$f3b==minf3b)])
+  { if (length(trif3b) > 1)
+    trialf3b = trif3b[-1]
+    else
+      ( trialf3b = trif3b)}    
+  f2bmatch = (tempar2$f2b[which(tempar2$trial==trialf3b)])
+  arf2bbase = median(subset(tempar2, trial<16)$f2b)
+  #    cat("For subject", subj, "here's the minimum f3b:", minf3b,"at trial", trial, "where f2b was", f2bmatch,"\n")
+  mindf3b$sub[i]<-(subj)
+  mindf3b$mintrialf3b[i]<-as.numeric(trialf3b)
+  mindf3b$minf3b[i]<-as.numeric(minf3b)
+  mindf3b$f2bmatch[i]<-as.numeric(f2bmatch)
+  mindf3b$arf2bbase[i]<-as.numeric(arf2bbase) # what is the basleine f2b in the f3b block
+  mindf3b$basef3b[i]<-as.numeric(mbf3b)
+}
+
+mindf3b<-data.frame(mindf3b)
+
+
+
+mindf3b$f3bmax<-(13*atan(0.00076*mindf3$f3max))+(3.5*atan((mindf3$f3max/7500)^2))
+
+mindf3b$f3bdiff<-mindf3b$basef3b-mindf3b$minf3b
+mindf3b$f2bdiff<-mindf3b$arf2bbase-mindf3b$f2bmatch
+mindf3b$pert<-mindf3b$f3bmax-mindf3b$basef3b
+
+mindf3b$f3bperc<-mindf3b$f3bdiff/mindf3b$pert
+
+mindf3b$oppf2bd<-mindf3b$f2bmatch-mindf3b$arf2bbase
+
 
 
 
@@ -123,5 +173,6 @@ points(f3~trial, subset(tam23rds, sub=='236' & buffer == 'outbuf'), col = 'red')
 plot(f3~trial, subset(tam23rds, sub=='237' & buffer == 'inbuf'))
 plot(f3~trial, subset(tam23rds, sub=='237' & buffer == 'inbuf'), ylim=c(2200,2700))
 
-
+india_alphas<-read.delim('India_Diss_palates.csv', sep = ',', header = TRUE)
+mindf3alph<-merge(mindf3,india_alphas, by = 'sub')
 
